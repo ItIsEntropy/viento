ee974ce1c5b5d4bd3b352c6583710de9515a6deb
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.com>
2022-03-21 16:33:12 +0200

    empty branch for posix added

--- mortice.py
+++ mortice.py
@@ -1,6 +1,7 @@
 # Create a lock for POSIX or NT systems and present a platform agnostic API to users.
 
 import os
+import msvcrt
 from pathlib import Path
 from typing import NoReturn, Callable
 
@@ -8,11 +9,12 @@ def lock_file(func: Callable[..., Path]) -> Path :
     def wrapper():
         open_file = func(*args, **kwargs)
         if os.name == 'nt':
-            import win32con, win32file, pywintypes
-            LOCK_EX = win32con.LOCKFILE_EXCLUSIVE_LOCK
-            LOCK_SH = 0 # Shared lock is default lock
-            LOCK_NB = win32con.LOCKFILE_FAIL_IMMEDIATELY
-            overlapped = pywintypes.OVERLAPPED()
+            lock_file = msvcrt.locking(open_file, msvcrt.LK_LOCK,0)
+            pass
+        elif os.name == 'posix':
+            pass
+        else:
+            raise RuntimeError('Mortice currently only has support for NT and POSIX based platforms')
     return wrapper
 
 def unlock_file(func) -> NoReturn :

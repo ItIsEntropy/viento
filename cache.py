54dfd8bfdaf19f38b83e7e6eed8f254de9cfd725
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.com>
2022-04-12 12:26:27 +0200

    improve docs, add url param

--- cache.py
+++ cache.py
@@ -1,12 +1,15 @@
-from util.net import get_five_day_forecast
+from util.net import get_weather_data
 from typing import Any
 
-async def get_forecast(latitude: str, longitude: str) -> dict[str, dict[str, Any]] | Exception:
+async def get_forecast(latitude: str, longitude: str, url: str) -> dict[str, dict[str, Any]] | Exception:
     '''
     Get the forecast of a location based on the coordinates provided.
     The data is acquired from the cache file if it is present and contains the data, or the weather API otherwise
-    :param latitude:
-    :param longitude:
-    :return:
+    :param latitude: latitude of location to query
+    :param longitude: longitude of location to query
+    :return: a response or an APIError
     '''
-    pass # TODO: add a radius checking function to see if latlong within range of present latlongs
+    # TODO get cache file and check for url match
+    # TODO: if url matches return data, else get data from the web using API call
+    # TODO: save the returned API data with the URL. start a 10 minute timer to delete the data
+    pass

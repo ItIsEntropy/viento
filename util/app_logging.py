277508c6c1e828103a1e7d7ab62d19cd539927e5
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.comGIT_COMMITTER_EMAIL=29229275+ItIsEntropy@users.noreply.github.com>
2022-04-12 12:07:37 +0200

    collect plugons for loggin and file locks. use setting for lock selection

---
+++ util/app_logging.py
@@ -0,0 +1,14 @@
+import importlib.metadata
+import logging
+
+
+async def get_handlers() -> list[logging.Handler]:
+    '''
+    Check installed entrypoints fo a log handler that advertises itself as capabe of working with vient via the 
+    `viento.log_handlers` entrypoint and returns them in a list
+    '''
+    # get all viento.log_handlers entrypoints, and make a list of log handlers from it
+    handlers: dict[str, logging.Handler] = [
+        entrypoint for entrypoint in tuple(importlib.metadata.entry_points().select()['viento.log_handlers'])
+    ]
+    return handlers
--- util/io.py
+++ util/io.py
@@ -1,18 +1,30 @@
 # TODO: create logic to make a file given a file name.
 # TODO: make a bg task that deletes the file after 10 minutes.
 # TODO: if file is locked then repeat after one sec ad infinitum till the file is deleted.
-
-
-from importlib.metadata import entry_points
+import importlib.metadata
+import io
+import os
+import yaml
+from pathlib import Path
 from typing import Any
+from decouple import Config # TODO: replace with yaml configs
 
-async def get_file_locker() -> dict[str, Any]:
+async def get_file_locker() -> Any:
     '''
     Checks installed entrypoints for a file locking context manager that's registered to work with viento.
     '''
-    # TODO make empty dict of entrypoints for lockers
-    # TODO: add lockers to the dict by entrypoint name
-    lockers: dict[str, Any]
-    for entry_point in entry_points('viento.file_lockers'):
-        lockers[entry_point.name] = entry_point
-    return lockers
+    # get all viento.filelocker entrypoints, and make a dictionary of `{locker_name: file_locker}`
+    lockers: dict[str, Any] = {
+        entrypoint.name: entrypoint for entrypoint in tuple(importlib.metadata.entry_points().select()['viento.file_lockers'])
+    }
+    working_dir: Path = Path(os.getcwd())
+    with open(working_dir.joinpath('config.yaml'), 'r') as config_file:
+        locker_setting: str = yaml.load(config_file)['file_locker']
+        locker_name: str = 'mortice' if locker_setting is None else locker_setting
+        try:
+            return lockers[locker_name].load()
+        except KeyError:
+            msg = f'File locker {locker_name} is not available.\navailabler file lockers: {", ".join(sorted(lockers))}'
+            print(msg) # TODO: Log this properly
+        raise ValueError(msg)
+        

b81bdd2df26927d7088f7d14da6267246bbc0746
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.comGIT_COMMITTER_EMAIL=29229275+ItIsEntropy@users.noreply.github.com>
2022-04-12 13:56:37 +0200

    switching to more eneric caching strategy

--- cache.py
+++ cache.py
@@ -1,7 +1,16 @@
 from util.net import get_weather_data
-from typing import Any
+from typing import Any, Callable
 
-async def get_forecast(latitude: str, longitude: str, url: str) -> dict[str, dict[str, Any]] | Exception:
+def cached(func) -> Callable[[Callable], Callable]:
+    def cache_or_net(*a,**kw):
+        url: str = kw['url']
+        # TODO: shorten latlong
+        # TODO: key cache dict by dict[url, dict[lat,long, cached data]]
+        cached_value = func(*a,**kw)
+        # TODO: if cached value is None, make call to net and return that instead and save value it in cache
+    return cache_or_net
+
+def get_forecast(latitude: str, longitude: str) -> dict[str, dict[str, Any]] | Exception:
     '''
     Get the forecast of a location based on the coordinates provided.
     The data is acquired from the cache file if it is present and contains the data, or the weather API otherwise
--- util/net.py
+++ util/net.py
@@ -10,7 +10,7 @@ API_KEY: str = config('WIND_API_KEY')  # API key for the openweather API
 async def get_weather_data(
         latitude: str,
         longitude: str,
-        call_type: str = 'current'
+        url: str
 ) -> dict[str, dict[str, Any]] | APIError:
     """
     Get the 5 day, 3hour forecast for the area specified by latitude and longitude.
@@ -19,10 +19,7 @@ async def get_weather_data(
     :param call_type: The type of call (forecast data type) to make
     :return: a dictionary (the JSON response of the API) or an Exception
     """
-    call_types: dict[str, str] = {'current': 'weather', 'forecast': 'forecast'}
-    if call_type not in call_types.keys():
-        ValueError(f"Unable to make weather forecast type. valid types are:{call_types.keys()}") # invalid forecast type wanted
-    url: str = f'https://api.openweathermap.org/data/2.5/{call_types[call_type]}'
+    # = f'https://api.openweathermap.org/data/2.5/{call_types[call_type]}'
     query_params: dict = {'lat': latitude, 'lon': longitude, 'appid': API_KEY}
     response: requests.Response = requests.get(url=url, params=query_params)
     match response.status_code:  # Check the status of the request

0f92de591a4d2bcb603d3797b88404bddf1c9066
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.comGIT_COMMITTER_EMAIL=29229275+ItIsEntropy@users.noreply.github.com>
2022-04-13 02:15:26 +0200

    modularise

--- webapp.py
+++ webapp.py
@@ -5,6 +5,15 @@ from util.exceptions import APIError, APILimitError
 
 app = FastAPI()  # Import the app
 
+async def make_call(latitude: str, longitude: str, url:str, err_msg: str):
+    try:
+        response: dict[str, dict[str, Any]] = await get_weather_data(latitude = latitude, longitude = longitude, url = url)
+    except (OSError, APIError, APILimitError):
+        # TODO: Make an error 50x
+        pass
+    # TODO: return 
+    
+
 
 @app.get('/')
 async def get_home(latitude: str | None = None, longitude: str | None = None):
@@ -24,15 +33,14 @@ async def get_forecast(latitude: str, longitude: str):
     :return: None
     '''
     url: str = 'https://api.openweathermap.org/data/2.5/forecast'
-    try:
-        response: dict[str, dict[str, Any]] = get_weather_data(latitude = latitude, longitude = longitude, url = url)
-    except (OSError, APIError, APILimitError):
-        # TODO: Make an error 50x
-        pass
-    # TODO: return 
+    err_msg: str = f'Something went wrong!!'
+    make_call(latitude=latitude, longitude=longitude, url=url)
 
 @app.get('/current')
 async def get_current_weather(latitude: str, longitude: str):
     # Get the current weather of the caller's location
     url: str = 'https://api.openweathermap.org/data/2.5/weather'
+    err_msg: str = f'Something went wrong rn!!'
+    make_call(latitude=latitude, longitude=longitude, url=url)
+
 

88bcc6ff2aa54892fbd3a83af7345cda7a0a3455
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.comGIT_COMMITTER_EMAIL=29229275+ItIsEntropy@users.noreply.github.com>
2022-04-13 02:07:42 +0200

    improve aliases

--- util/typing.py
+++ util/typing.py
@@ -1,5 +1,5 @@
 # type aliases
 WeatherData = dict[str, dict] # TODO: fill dict with correct type after parsing json response
-LocationData = dict[str, WeatherData | float] 
-URLData = dict[str, LocationData]
-DataCache = dict[str, URLData]
+TimeStamp = float
+LocationData = dict[str, WeatherData | TimeStamp] 
+DataCache = dict[str, LocationData]

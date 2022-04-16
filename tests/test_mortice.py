e72122a175ee20cca893e71f20137336c492a6bf
ItIsEntropy <29229275+ItIsEntropy@users.noreply.github.com>
2022-04-16 03:48:09 +0200

    fiz asserts, swithc to yielding fixtures with auto cleanup

--- tests/test_mortice.py
+++ tests/test_mortice.py
@@ -12,8 +12,10 @@ def create_temp_file() -> io.TextIOWrapper:
     Fixture that creates temporary file
     :return: a temporary io.TextIOWrapper object
     """
-    test_file: io.TextIOWrapper = tempfile.TemporaryFile()
-    return test_file
+    test_file: io.TextIOWrapper = tempfile.TemporaryFile()#dir=Path(os.getcwd()))
+    test_file.write(b"This is nonsense text. exists so we can lock the file :)")
+    yield test_file
+    os.remove(test_file)
 
 @pytest.fixture
 def lock_file(create_temp_file) -> io.TextIOWrapper:
@@ -28,17 +30,17 @@ def lock_file(create_temp_file) -> io.TextIOWrapper:
         mode: int = msvcrt.LK_NBLCK
         n_bytes: int = locked_file.seek(0, 2)
         msvcrt.locking(locked_file.fileno(), mode, n_bytes)
-        return locked_file
+        yield locked_file
     elif os.name == 'posix':
         import fcntl
         mode: int = fcntl.LOCK_EX | fcntl.LOCK_NB
         fcntl.flock(locked_file.fileno(), mode)
-        return locked_file
+        yield locked_file
 
-deadlock_error: dict[str, int] = {'nt': 36, 'posix': 26}# TODO: get POSIX number
+lock_error: dict[str, int] = {'nt': [36, 13], 'posix': 26}# TODO: get POSIX number
 
 # TODO: lock with app and then use non blocking mode in a fixture to ensure OSError is raised.
-def test_context(lock_file, create_temp_file) -> None:
+def test_context(lock_file) -> None:
     """
     Test the context manager mode
     :return: None
@@ -46,7 +48,7 @@ def test_context(lock_file, create_temp_file) -> None:
     with pytest.raises(Exception) as e:
         with Mortice(file_path=lock_file, mode='w', blocking=False):
             pass
-    assert e.value.errno == deadlock_error[os.name]
+    assert e.value.errno in lock_error[os.name]
 
 @pytest.mark.anyio
 async def test_async_context(lock_file) -> None:
@@ -57,7 +59,7 @@ async def test_async_context(lock_file) -> None:
     with pytest.raises(Exception) as e:
         async with Mortice(file_path=lock_file, mode='w', blocking=False):
             pass
-    assert e.value.errno == deadlock_error[os.name]
+    assert e.value.errno in lock_error[os.name]
 
 def test_lock(lock_file) -> None:
     """
@@ -66,10 +68,14 @@ def test_lock(lock_file) -> None:
     """
     with pytest.raises(OSError) as e:
         Mortice.lock_file(lock_file)
-    assert e.value.errno == deadlock_error[os.name]
-def tet_unlock() -> None:
+    assert e.value.errno in lock_error[os.name]
+def test_unlock(lock_file, create_temp_file) -> None:
     """
     test unlock API
     :return: None
     """
+    Mortice.unlock_file(lock_file)
+    with pytest.raises(OSError) as e:
+        Mortice.unlock_file(create_temp_file)
+    assert e.value.errno in lock_error[os.name]
     pass #TODO use fixture to unlock and test that app raises an error when called or vice versa
